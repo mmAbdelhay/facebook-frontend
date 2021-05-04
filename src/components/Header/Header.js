32ad58cd40a8420137c6f85b650d8e3663af2972
mmAbdelhay <abdelhay.courses@gmail.com>
2021-05-04 03:08:36 +0200

    conditional login and logout

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,44 +1,62 @@
 import React from "react";
-import { Link } from "react-router-dom";
-import { Button, Form, FormControl, Nav, Navbar } from "react-bootstrap";
+import {Link} from "react-router-dom";
+import {Button, Form, FormControl, Nav, Navbar} from "react-bootstrap";
 import {checkIfLoggedIn} from "../../Service/CheckUserStatus";
 
 class Header extends React.Component {
 
-    logout = ()=>{
+    constructor() {
+        super();
+        this.state = {
+            loginStatus: false,
+        }
+    }
+
+    componentDidMount() {
         const [loginStatus, loginToken] = checkIfLoggedIn();
         if (loginStatus) {
+            this.setState({loginStatus: true});
+        }
+    }
+
+    logout = () => {
+        if (this.state.loginStatus) {
             localStorage.clear();
             window.location.href = '/login'
         }
     }
 
     render() {
-      return (
-         <Navbar bg="dark" variant="dark" className="mb-4">
-            <Navbar.Brand href="/">Facebook</Navbar.Brand>
-            <Nav className="mr-auto">
-               <a href="/" className="nav-item nav-link">
-                  News Feed
-               </a>
-               <Link to="/profile" className="nav-item nav-link">
-                  Profile
-               </Link>
-               <Link to="/login" className="nav-item nav-link">
-                  Login
-               </Link>
-               <Link to="/signup" className="nav-item nav-link">
-                  Signup
-               </Link>
-                <Link className="nav-item nav-link" onClick={this.logout}>logout</Link>
-            </Nav>
-            <Form inline>
-               <FormControl type="text" placeholder="Search" className="mr-sm-2" />
-               <Button variant="outline-info">Search</Button>
-            </Form>
-         </Navbar>
-      );
-   }
+        return (
+            <Navbar bg="dark" variant="dark" className="mb-4">
+                <Navbar.Brand href="/">Facebook</Navbar.Brand>
+                <Nav className="mr-auto">
+                    <a href="/" className="nav-item nav-link">
+                        News Feed
+                    </a>
+                    <Link to="/profile" className="nav-item nav-link">
+                        Profile
+                    </Link>
+                    {!this.state.loginStatus ?
+                        <>
+                            <Link to="/login" className="nav-item nav-link">
+                                Login
+                            </Link>
+                            <Link to="/signup" className="nav-item nav-link">
+                                Signup
+                            </Link>
+                        </>
+                        :
+                        <Link className="nav-item nav-link" onClick={this.logout}>logout</Link>
+                    }
+                </Nav>
+                <Form inline>
+                    <FormControl type="text" placeholder="Search" className="mr-sm-2"/>
+                    <Button variant="outline-info">Search</Button>
+                </Form>
+            </Navbar>
+        );
+    }
 }
 
 export default Header;
